d8c3214db2add545d2d9b0aa2a81cf59de5d12bf
Alena Labuko <79700929+A-lyona@users.noreply.github.com>
2025-09-19 22:54:41 +0300

    feat: add angle

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -111,8 +111,11 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const line = x1 * x2 + y1 * y2;
+  const rootOne = Math.sqrt(x1 * x1 + y1 * y1);
+  const rootTwo = Math.sqrt(x2 * x2 + y2 * y2);
+  return Math.acos(line / (rootOne * rootTwo));
 }
 
 /**
