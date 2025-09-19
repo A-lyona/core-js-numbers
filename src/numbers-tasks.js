f9541c0e662c4250a4ee40dba9584d0e7b9ec53c
Alena Labuko <79700929+A-lyona@users.noreply.github.com>
2025-09-19 22:41:01 +0300

    feat: add distance

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -71,8 +71,11 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const lineY = (y1 - y2) ** 2;
+  const lineX = (x1 - x2) ** 2;
+  const result = Math.sqrt(lineY + lineX);
+  return result;
 }
 
 /**
